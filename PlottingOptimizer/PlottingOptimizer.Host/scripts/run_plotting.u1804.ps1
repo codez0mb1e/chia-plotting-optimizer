da1463700f57b6577e350ef8047a6c96ed743c53
codez0mb1e <d.petukhov@outlook.com>
2021-05-23 16:38:11 +0000

    Minor fixes

--- PlottingOptimizer/PlottingOptimizer.Host/scripts/run_plotting.u1804.ps1
+++ PlottingOptimizer/PlottingOptimizer.Host/scripts/run_plotting.u1804.ps1
@@ -8,11 +8,8 @@
   [string]$chiaVersion
 )
 
-Set-Location ~/chia-blockchain
-sh ./activate
-
+Set-Alias -Name chia -Value \home\dictator\chia-blockchain\venv\bin\chia
 Write-Host "Chia version:" (chia version)
-
 
 function Get-PlotterLogPath { 
   $date = Get-date -format yyyy-MM-ddThh-mm-ss
@@ -22,7 +19,5 @@ function Get-PlotterLogPath {
   $path
 }
 
-$logPath = Get-PlotterLogPath
-
 # chia plots create -k 32 -n 1 -b 5000 -r 2 -t /plotdrive1 -d /harvestdrive1 -f <public_framer_key> 2>&1 | tee ~/chia-blockchain/logs/q1t2_1.log
-chia plots create -n $plotsPerQueue -b $buffer -r $threads -t $tempDir -d $finalDir | Tee-Object -FilePath $logpath
+chia plots create -n $plotsPerQueue -b $buffer -r $threads -t $tempDir -d $finalDir | Tee-Object -FilePath (Get-PlotterLogPath)
