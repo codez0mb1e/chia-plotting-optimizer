f05bbbe5ada075883b7aaae11a2cacdb7c4ba7b3
codez0mb1e <d.petukhov@outlook.com>
2021-05-26 21:02:44 +0000

    Refactor run plotting script

--- PlottingOptimizer/PlottingOptimizer.Host/scripts/run_plotting.u1804.ps1
+++ PlottingOptimizer/PlottingOptimizer.Host/scripts/run_plotting.u1804.ps1
@@ -8,8 +8,6 @@
   [string]$chiaVersion
 )
 
-Set-Alias -Name chia -Value \home\dictator\chia-blockchain\venv\bin\chia
-Write-Host "Chia version:" (chia version)
 
 function Get-PlotterLogPath { 
   $date = Get-date -format yyyy-MM-ddThh-mm-ss
@@ -19,5 +17,9 @@ function Get-PlotterLogPath {
   $path
 }
 
+Set-Alias -Name chia -Value \home\dictator\chia-blockchain\venv\bin\chia
+Write-Host "Chia version:" (chia version)
+
+chia plots create -k 32 -n 1 -b 5000 -r 2 -t /plotdrive1 -d /harvestdrive1 2>&1 | tee ~/chia-blockchain/logs/2021-05-24_q1t2_1.log
 # chia plots create -k 32 -n 1 -b 5000 -r 2 -t /plotdrive1 -d /harvestdrive1 -f <public_framer_key> 2>&1 | tee ~/chia-blockchain/logs/q1t2_1.log
-chia plots create -n $plotsPerQueue -b $buffer -r $threads -t $tempDir -d $finalDir | Tee-Object -FilePath (Get-PlotterLogPath)
+# chia plots create -n $plotsPerQueue -b $buffer -r $threads -t $tempDir -d $finalDir | Tee-Object -FilePath (Get-PlotterLogPath)
