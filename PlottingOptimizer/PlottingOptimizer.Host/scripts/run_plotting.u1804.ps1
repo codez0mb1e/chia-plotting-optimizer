e2fa519bc9e5ff28b909df2a3e4b8a5fa308c6a0
codez0mb1e <d.petukhov@outlook.com>
2021-05-29 16:39:47 +0000

    Add farmerKey to params list

--- PlottingOptimizer/PlottingOptimizer.Host/scripts/run_plotting.u1804.ps1
+++ PlottingOptimizer/PlottingOptimizer.Host/scripts/run_plotting.u1804.ps1
@@ -5,7 +5,8 @@
   [string]$tempDir,
   [string]$finalDir,
   [string]$logDir,
-  [string]$chiaVersion
+  [string]$chiaVersion,
+  [string]$farmerKey
 )
 
 
@@ -20,6 +21,4 @@ function Get-PlotterLogPath {
 Set-Alias -Name chia -Value \home\dictator\chia-blockchain\venv\bin\chia
 Write-Host "Chia version:" (chia version)
 
-chia plots create -k 32 -n 1 -b 5000 -r 2 -t /plotdrive1 -d /harvestdrive1 2>&1 | tee ~/chia-blockchain/logs/2021-05-24_q1t2_1.log
-# chia plots create -k 32 -n 1 -b 5000 -r 2 -t /plotdrive1 -d /harvestdrive1 -f <public_framer_key> 2>&1 | tee ~/chia-blockchain/logs/q1t2_1.log
-# chia plots create -n $plotsPerQueue -b $buffer -r $threads -t $tempDir -d $finalDir | Tee-Object -FilePath (Get-PlotterLogPath)
+chia plots create -n $plotsPerQueue -b $buffer -r $threads -t $tempDir -d $finalDir -f $farmerKey | Tee-Object -FilePath (Get-PlotterLogPath)
